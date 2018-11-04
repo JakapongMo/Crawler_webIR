e61394cc55d0bab205f84fda64bc381e0fa35ea3
olan <olantoj@hotmail.com>
2018-11-04 15:04:23 +0700

    filter others

--- test.py
+++ test.py
@@ -2,35 +2,53 @@ import requests as r
 from bs4 import BeautifulSoup
 
 headers = {
-        'User-Agent': 'haha',
-        'From': 'haha'
+        'User-Agent': 'WebIR Crawler',
+        'From': 'Kasetsart University'
         }
 
 a = r.get("https://www.metacritic.com/game/xbox-one/red-dead-redemption-2/", headers=headers)
+critic = r.get("https://www.metacritic.com/game/xbox-one/red-dead-redemption-2/critic-reviews")
 
 # f = open("test.html", "w")
+# f.write(a.text)
+# f.close()
+
+# f = open("critic.html", "w")
 # f.write(a.text)
 # f.close()
 
 f = open("test.html", "r")
 a = f.read()
 f.close()
-#print(a)
+
+f = open("critic.html", "r")
+critic = f.read()
+f.close()
 
 soup = BeautifulSoup(a, "html.parser")
 
 name = soup.find("a", {"class" : ["hover_none"]})
-print(name.string.strip())
+name = name.find("h1").string.strip()
+print(name)
+
+platform = soup.find("span", {"class" : ["platform"]})
+platform = platform.find("a").string.strip()
+print(platform)
 
 dev = soup.find("li", {"class" : ["summary_detail developer"]})
-print(dev.find("span", {"class" : "data"}).string.strip())
+dev = dev.find("span", {"class" : "data"}).string.strip()
+print(dev)
+
+summary = soup.find("span", {"class" : ["blurb blurb_expanded"]})
+summary = summary.string.strip()
+print(summary)
 
 data = {}
-data["Name"] = ""
-data["Platform"] = ""
+data["Name"] = name
+data["Platform"] = platform
 data["Genre"] = ""
 data["Developer"] = dev
-data["Summary"] = ""
+data["Summary"] = summary
 data["Review"] = {}
 
 f = open("dict.json", "w")
