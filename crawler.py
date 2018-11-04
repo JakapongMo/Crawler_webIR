465cc095da2b5c35cc8e333a0bff06645ac75de9
olan <olantoj@hotmail.com>
2018-11-04 17:16:15 +0700

    extract_metascore()

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import Filterer as ftr
 
-def make_json(name, platform, genre, developer, summary, review, source, url):
+def make_json(name, platform, genre, developer, summary, metascore, review, source, url):
     data = {}
     data["Name"] = name
     data["Platform"] = platform
@@ -40,35 +40,37 @@ for url in urls:
     url = url.strip()
     critic_url = url + "/critic-reviews"
 
-    detail = get_page(url)
-    critic = get_page(critic_url)
+    detail, detail_status = get_page(url)
+    critic, critic_status = get_page(critic_url)
 
-    detail = BeautifulSoup(detail, "html.parser")
+    if detail_status == 200 and critic_status == 200:
+        detail = BeautifulSoup(detail, "html.parser")
 
-    name = ftr.extract_name(detail)
-    platform = ftr.extract_platform(detail)
-    dev = ftr.extract_developer(detail)
-    summary = ftr.extract_summary(detail)
-    genre = ftr.extract_genre(detail)
-    score = ftr.extract_metascore(detail)
+        name = ftr.extract_name(detail)
+        platform = ftr.extract_platform(detail)
+        dev = ftr.extract_developer(detail)
+        summary = ftr.extract_summary(detail)
+        genre = ftr.extract_genre(detail)
+        score = ftr.extract_metascore(detail)
 
-    critic = BeautifulSoup(critic, "html.parser")
+        critic = BeautifulSoup(critic, "html.parser")
 
-    review = ftr.extract_review(critic)
-    source = ftr.extract_source(critic)
-    url = ftr.extract_url(critic)
+        review = ftr.extract_review(critic)
+        source = ftr.extract_source(critic)
+        url = ftr.extract_url(critic)
 
-    print(name)
-    print(platform)
-    print(dev)
-    print(summary)
-    print(genre)
-    print(review)
-    print(source)
-    print(url)
+        print(name)
+        print(platform)
+        print(dev)
+        print(summary)
+        print(genre)
+        print(metascore)
+        print(review)
+        print(source)
+        print(url)
 
-    data = make_json(name, platform, genre, dev, summary, review, source, url)
+        data = make_json(name, platform, genre, dev, summary, metascore, review, source, url)
 
-    f = open("dict.json", "w")
-    f.write(str(data))
-    f.close()
+        f = open("dict.json", "w")
+        f.write(str(data))
+        f.close()
