da3b8853b9a8e0b509c57619efa4836c893a2e7a
tengmo3210 <tengmo02@gmail.com>
2018-11-04 16:13:12 +0700

    get review content

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ headers = {
         }
 
 a = r.get("https://www.metacritic.com/game/xbox-one/red-dead-redemption-2/", headers=headers)
-critic = r.get("https://www.metacritic.com/game/xbox-one/red-dead-redemption-2/critic-reviews")
+critic = r.get("https://www.metacritic.com/game/xbox-one/red-dead-redemption-2/critic-reviews", headers=headers)
 
 # f = open("test.html", "w")
 # f.write(a.text)
 # f.close()
 
-# f = open("critic.html", "w")
-# f.write(a.text)
-# f.close()
+f = open("critic.html", "w")
+f.write(critic.text)
+f.close()
 
 f = open("test.html", "r")
 a = f.read()
@@ -25,6 +25,7 @@ f = open("critic.html", "r")
 critic = f.read()
 f.close()
 
+'''
 soup = BeautifulSoup(a, "html.parser")
 
 name = soup.find("a", {"class" : ["hover_none"]})
@@ -51,14 +52,38 @@ summary = soup.find("span", {"class" : ["blurb blurb_expanded"]})
 summary = summary.string.strip()
 print(summary)
 
+'''
 data = {}
+'''
 data["Name"] = name
 data["Platform"] = platform
 data["Genre"] = list_genre
 data["Developer"] = dev
 data["Summary"] = summary
+'''
 data["Review"] = {}
 
+#print (critic)
+soup_critic = BeautifulSoup(critic, "html.parser")
+
+
+
+first_review = soup_critic.find_all("li", {"class" : ["review critic_review first_review"]})
+print (len(first_review))
+
+reviews = soup_critic.find_all("li", {"class" : ["review critic_review"]})
+#first_review = review.findChildren("li" , class_= "review critic_review first_review",recursive=False)
+print (len(reviews))
+'''
+for review in reviews:
+        print(review)
+        print ("====================================")
+print (len(reviews))
+'''
+
+
+last_review = soup_critic.find_all("li", {"class" : ["review critic_review last_review"]})
+print (last_review)
 f = open("dict.json", "w")
 f.write(str(data))
 f.close()
